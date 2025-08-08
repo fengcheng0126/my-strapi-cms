d83dc67dca11063f9903c4055839a6792c0ca965
fengcheng0126 <fengcheng0126@gmail.com>
2025-08-08 12:41:30 -0500

    added firebase cors link

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -18,7 +18,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:4242', 'http://127.0.0.1:4242', "https://20b943945eea.ngrok-free.app"],
+      origin: ['http://localhost:4242', 'http://127.0.0.1:4242', "https://str333ak-pcs.web.app/"],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: ['Content-Type', 'Authorization'],
       credentials: true,
